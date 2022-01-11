341d53f9a1cf1ca4f81598f1846744e15f8f475e
Adria Tavares <adria.tavares28@gmail.com>
2022-01-11 19:42:37 -0300

    Deletando os arquivos .css dos components e estilizando-os com styled-components

--- modulo2/props-labedin/labedin/src/components/CardGrande/CardGrande.js
+++ modulo2/props-labedin/labedin/src/components/CardGrande/CardGrande.js
@@ -1,15 +1,42 @@
 import React from 'react';
-import './CardGrande.css'
+// import './CardGrande.css'
+
+import styled from 'styled-components';
+
+  const BigCardContainer = styled.div`
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+    padding: 20px 10px;
+    margin-bottom: 10px;
+    height: 200px;
+
+    img {
+      width: 70px;
+      margin-right: 10px;
+      border-radius: 50%;
+    }
+`
+
+const NomeDescricao = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-items: flex-start;
+
+    h4 {
+        margin-bottom: 15px;
+    }
+`
 
 function CardGrande(props) {
     return (
-        <div className="bigcard-container">
+        <BigCardContainer>
             <img src={ props.imagem } />
-            <div>
+            <NomeDescricao>
                 <h4>{ props.nome }</h4>
                 <p>{ props.descricao }</p>
-            </div>
-        </div>
+            </NomeDescricao>
+        </BigCardContainer>
     )
 }
 
