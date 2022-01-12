caa2d444c3f0dca870b5d1fb4768d73378ce12fe
Adria Tavares <adria.tavares28@gmail.com>
2022-01-12 17:34:29 -0300

    Adicionando implementações do exercício insta4

---
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50?a=1'}
+          fotoPost={'https://picsum.photos/200/150?a=2'}
+        />
+
+        <Post
+          nomeUsuario={'adria'}
+          fotoUsuario={'https://picsum.photos/50/50?a=3'}
+          fotoPost={'https://picsum.photos/200/150?a=4'}
+        />
+
+        <Post
+          nomeUsuario={'indio'}
+          fotoUsuario={'https://picsum.photos/50/50?a=5'}
+          fotoPost={'https://picsum.photos/200/150?a=6'}
+        />
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
