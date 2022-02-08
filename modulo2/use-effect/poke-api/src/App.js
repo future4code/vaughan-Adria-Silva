ba9afd126b949c647cffbc21f6a0e999b24b8e56
Adria Tavares <adria.tavares28@gmail.com>
2022-02-08 15:05:31 -0300

    Substituindo componentDidMount no App.js

--- modulo2/use-effect/poke-api/src/App.js
+++ modulo2/use-effect/poke-api/src/App.js
@@ -1,8 +1,42 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import PokeCard from "./components/PokeCard/PokeCard";
 
 function App() {
+  const [pokeList, setPokeList] = useState([]);
+  const [pokeName, setPokeName] = useState("");
+
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
+
+  const getAllPokemons = async () => {
+    try {
+      const response = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=151");
+      setPokeList(response.data.results);
+    } catch (err) {
+      console.log(err);
+    };
+  };
+
+  const changePokeName = (event) => {
+    setPokeName(event.target.value);
+    console.log(pokeName)
+  };
+
   return (
     <div>
-      <p>Oi</p>
+      <select onChange={changePokeName}>
+          <option value={""}>Nenhum</option>
+          {pokeList.map(pokemon => {
+            return (
+              <option key={pokemon.name} value={pokemon.name}>
+                {pokemon.name}
+              </option>
+            );
+          })}
+        </select>
+        {pokeName && <PokeCard pokemon={pokeName} />}
     </div>
   );
 }
