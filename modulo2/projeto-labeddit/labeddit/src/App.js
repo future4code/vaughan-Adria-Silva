80ccea040172bc7a0ad1d98cdeca8fe0c80f2e27
Adria Tavares <adria.tavares28@gmail.com>
2022-02-21 17:12:51 -0300

    Criando páginas do projeto

--- modulo2/projeto-labeddit/labeddit/src/App.js
+++ modulo2/projeto-labeddit/labeddit/src/App.js
@@ -1,8 +1,16 @@
+import CreateAccountPage from "./pages/createAccountPage/CreateAccount";
+import FeedPage from "./pages/feedPage/FeedPage";
+import LoginPage from "./pages/loginPage/LoginPage";
+import PostPage from "./pages/postPage/PostPage";
 
 const App = () => {
   return (
     <div>
       Labeddit
+      <CreateAccountPage />
+      <FeedPage />
+      <LoginPage />
+      <PostPage />
     </div>
   );
 }
