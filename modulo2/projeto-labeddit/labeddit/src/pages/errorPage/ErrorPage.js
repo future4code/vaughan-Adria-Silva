745e1002a392ca809c9faa906c8e63e51207b906
Adria Tavares <adria.tavares28@gmail.com>
2022-02-23 09:49:14 -0300

    Página de erro

--- modulo2/projeto-labeddit/labeddit/src/pages/errorPage/ErrorPage.js
+++ modulo2/projeto-labeddit/labeddit/src/pages/errorPage/ErrorPage.js
@@ -1,7 +1,14 @@
 import React from "react";
+import error from "../../assets/error.png"
+import { MainContainer } from "./styles";
 
 const ErrorPage = () => {
-    return <div>ErrorPage</div>
+    return (
+        <MainContainer>
+            <img src={error} alt="Ícone de erro 404 "/>
+            <h4>Erro 404 - Página não encontrada</h4>
+        </MainContainer>
+    );
 };
 
 export default ErrorPage;
