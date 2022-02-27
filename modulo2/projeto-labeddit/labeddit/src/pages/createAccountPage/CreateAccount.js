5ce136c63417ed6143dba17ba304042a88812c03
Adria Tavares <adria.tavares28@gmail.com>
2022-02-27 10:15:34 -0300

    Estilo + responsividade da CreateAccountPage

--- modulo2/projeto-labeddit/labeddit/src/pages/createAccountPage/CreateAccount.js
+++ modulo2/projeto-labeddit/labeddit/src/pages/createAccountPage/CreateAccount.js
@@ -1,15 +1,36 @@
+import { Typography } from "@mui/material";
 import React from "react";
 import useUnProtectedPage from "../../hooks/useUnProtectedPage";
 import CreateAccountForm from "./components/CreateAccountForm";
+import styled from "styled-components";
+
+const MainContainer = styled.main`
+    height: calc(90vh - 64px);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    @media(max-width: 600px) {
+        height: calc(90vh - 56px);
+    }
+`
+
+const Title = styled(Typography)`
+    text-align: center;
+    text-shadow: 1px 1px 8px #3c3c3c;
+    margin: 16px;
+`
 
 const CreateAccountPage = ({setLogInOut}) => {
     useUnProtectedPage();
 
     return (
-        <div>
-            <h2>Crie uma conta</h2>
+        <MainContainer>
+            <Title variant='h1' color={"primary"}>labeddit</Title>
+            <Typography variant='h4' color={"text.primary"}>Crie uma conta</Typography>
             <CreateAccountForm setLogInOut={setLogInOut}/>
-        </div>
+        </MainContainer>
     );
 };
 
--- modulo2/projeto-labeddit/labeddit/src/pages/createAccountPage/components/CreateAccountForm.js
+++ modulo2/projeto-labeddit/labeddit/src/pages/createAccountPage/components/CreateAccountForm.js
@@ -3,6 +3,16 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import useForm from "../../../hooks/useForm";
 import { signUp } from "../../../services/users";
+import styled from "styled-components";
+
+const Form = styled.form`
+    width: 350px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+`
 
 const CreateAccountForm = ({setLogInOut}) => {
     const { form, onChangeForm, cleanFields } = useForm(
@@ -21,16 +31,19 @@ const CreateAccountForm = ({setLogInOut}) => {
     };
 
     return (
-            <form onSubmit={onSubmitForm}>
+            <Form onSubmit={onSubmitForm}>
                 <TextField
                     name={"username"}
                     type={"username"}
                     value={form.username}
                     onChange={onChangeForm}
                     label={"Nome de usuário(a)"}
+                    placeholder={"Nome"}
                     variant={"outlined"}
                     autoFocus
                     required
+                    margin={"normal"}
+                    fullWidth
                 />
                 <TextField
                     name={"email"}
@@ -38,9 +51,12 @@ const CreateAccountForm = ({setLogInOut}) => {
                     value={form.email}
                     onChange={onChangeForm}
                     label={"E-mail"}
+                    placeholder={"seu_email@exemplo.com"}
                     variant={"outlined"}
                     inputProps={{ pattern:"^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$", title:"seuemail@exemplo.com" }}
                     required
+                    margin={"normal"}
+                    fullWidth
                 />
                 <TextField
                     name={"password"}
@@ -48,10 +64,13 @@ const CreateAccountForm = ({setLogInOut}) => {
                     value={form.password}
                     onChange={onChangeForm}
                     label={"Senha"}
+                    placeholder={"********"}
                     variant={"outlined"}
                     helperText={"Mín 8 e máx 30 caracteres"}
                     inputProps={{ pattern:"^[^ ]{8,30}$", title:"Mínimo 8 e máx 30 caracteres sem espaço" }}
                     required
+                    margin={"normal"}
+                    fullWidth
                 />
                 <Button
                     type={"submit"}
@@ -60,7 +79,7 @@ const CreateAccountForm = ({setLogInOut}) => {
                 >
                     Criar conta
                 </Button>
-            </form>
+            </Form>
     );
 };
 
