e3095a8ab05eb73bc47cda737b4f92e1643f9ab8
Adria Tavares <adria.tavares28@gmail.com>
2022-03-22 12:22:04 -0300

    Exercício 1 - Item b

--- modulo4/node-package-json/index.js
+++ modulo4/node-package-json/index.js
@@ -11,3 +11,9 @@
 //a seguinte estrutura: "Olá, (Nome)! Você tem (sua idade) anos."
 
     // Resposta:
+    const printMessage = () => {
+        const name = process.argv[2];
+        const age = process.argv[3];
+        console.log(`Olá, ${name}! Você tem ${age} anos.`)
+    }
+    printMessage()
