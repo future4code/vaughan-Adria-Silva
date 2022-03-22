a5c522756dc98f16ef9e4bd0913a895956e40d86
Adria Tavares <adria.tavares28@gmail.com>
2022-03-22 14:31:51 -0300

    Exercicio4

--- modulo4/node-package-json/Exercicio1/index.js
+++ modulo4/node-package-json/Exercicio1/index.js
@@ -27,34 +27,3 @@
         console.log(`Olá, ${name}! Você tem ${age} anos. Em sete anos você terá ${futureAge}.`);
     };
     printNewMessage();
-
-// Exercício 2
-
-//Crie uma aplicação Node que recebe uma string representando uma operação 
-//matemática e dois valores numéricos. O retorno deverá ser o resultado da 
-//operação selecionada utilizando os 2 valores fornecidos.
-
-    const mathOperation = () => {
-        const operation  = process.argv[2];
-        const firstNumber  = Number(process.argv[3]);
-        const secondNumber  = Number(process.argv[4]);
-
-        let result = 0
-        switch (operation) {
-            case "add":
-                result = firstNumber + secondNumber;
-                break;
-            case "sub":
-                result = firstNumber - secondNumber;
-                break;
-            case "mult":
-                result = firstNumber * secondNumber;
-                break;
-            default:
-                result = firstNumber / secondNumber;
-                break; 
-        }
-
-        console.log("Resposta: ", result);
-        
-    }
