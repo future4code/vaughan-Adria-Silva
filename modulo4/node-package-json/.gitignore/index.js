9c946a162510a5f2baa99c497c81fcd24bff2e59
Adria Tavares <adria.tavares28@gmail.com>
2022-03-22 12:05:06 -0300

    Exercício 1 - item a

---
+++ modulo4/node-package-json/.gitignore/index.js
@@ -0,0 +1,13 @@
+// Exercício 1
+
+// a) Como fazemos para acessar os parâmetros passados na linha de comando 
+//para o Node?
+
+    //Resposta: process.argv[2], process.argv[3], o índice depende de 
+    //qual algumento deseja acessar.
+
+// b) Crie um programa que receba seu nome e sua idade. 
+// Após receber estes valores, imprima no console uma mensagem que siga 
+//a seguinte estrutura: "Olá, (Nome)! Você tem (sua idade) anos."
+
+    // Resposta:
