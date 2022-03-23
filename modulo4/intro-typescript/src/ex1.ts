1b6a73bd3245e38c16be52cc0f1f8f847b78350d
Adria Tavares <adria.tavares28@gmail.com>
2022-03-23 14:38:18 -0300

    Separando os esercícios

---
+++ modulo4/intro-typescript/src/ex1.ts
@@ -0,0 +1,23 @@
+// Exercício 1
+
+// function checaTriangulo(a, b, c) {
+//     if (a !== b && b !== c) {
+//       return "Escaleno";
+//     } else if (a === b && b === c) {
+//       return "Equilátero";
+//     } else {
+//       return "Isósceles";
+//     }
+//   }
+
+function checaTriangulo(a:number, b:number, c:number):string {
+    if (a !== b && b !== c) {
+      return "Escaleno";
+    } else if (a === b && b === c) {
+      return "Equilátero";
+    } else {
+      return "Isósceles";
+    };
+};
+
+console.log(checaTriangulo(8, 5, 7));
