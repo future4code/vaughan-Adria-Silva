2fbc2eb69e53bd13089b084e976f78ad124c03a1
Adria Tavares <adria.tavares28@gmail.com>
2022-03-23 12:18:40 -0300

    Exercício 4

--- modulo4/intro-typescript/src/index.ts
+++ modulo4/intro-typescript/src/index.ts
@@ -69,3 +69,44 @@ console.log(checaTriangulo(8, 5, 7));
   };
 
   console.log(checaAnoBissexto(2016));
+
+// Exercício 4
+
+// A seguinte função recebe dois números como parâmetro e retorna a diferença 
+//entre o maior número e o menor. Dessa forma, refatore a função para o Typescript.
+
+// function comparaDoisNumeros(num1, num2) {
+//   let maiorNumero;
+//   let menorNumero;
+
+//   if (num1 > num2) {
+//     maiorNumero = num1;
+//     menorNumero = num2;
+//   } else {
+//     maiorNumero = num2;
+//     menorNumero = num1;
+//   }
+
+//   const diferenca = maiorNumero - menorNumero;
+
+//   return diferenca 
+// }
+
+  function comparaDoisNumeros(num1:number, num2:number):number {
+    let maiorNumero:number;
+    let menorNumero:number;
+
+    if (num1 > num2) {
+      maiorNumero = num1;
+      menorNumero = num2;
+    } else {
+      maiorNumero = num2;
+      menorNumero = num1;
+    };
+
+    const diferenca:number = maiorNumero - menorNumero;
+
+    return diferenca;
+  };
+
+  console.log(comparaDoisNumeros(500,1020));
