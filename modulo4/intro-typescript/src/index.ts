35efff712951fcec6392bab6677cae8c32baeabe
Adria Tavares <adria.tavares28@gmail.com>
2022-03-23 12:14:31 -0300

    Exercício 3

--- modulo4/intro-typescript/src/index.ts
+++ modulo4/intro-typescript/src/index.ts
@@ -26,3 +26,46 @@ function checaTriangulo(a:number, b:number, c:number):string {
 
 console.log(checaTriangulo(8, 5, 7));
 
+// Exercício 2
+
+// A seguinte função em JavaScript pergunta ao usuário suas três cores 
+//favoritas e imprime no console um array que contenha essas três cores. 
+//Refatore a função para o Typescript.
+
+// function imprimeTresCoresFavoritas() {
+//   const cor1 = prompt("Insira sua primeira cor favorita")
+//   const cor2 = prompt("Insira sua segunda cor favorita")
+//   const cor3 = prompt("Insira sua terceira cor favorita")
+//   console.log([cor1, cor2, cor3])
+// }
+
+  // function imprimeTresCoresFavoritas():void {
+  //   const cor1:string = prompt("Insira sua primeira cor favorita")
+  //   const cor2:string = prompt("Insira sua segunda cor favorita")
+  //   const cor3:string = prompt("Insira sua terceira cor favorita")
+  //   console.log([cor1, cor2, cor3])
+  // }
+
+//Exercício 3
+
+// A função recebe um ano e retorna um booleano (true ou false) que indica 
+//se o ano é bissexto. Um ano é bissexto de acordo com as seguintes condições:
+//  São bissextos todos os anos múltiplos de 400.
+//  São bissextos todos os múltiplos de 4, exceto se for múltiplo de 100 
+//mas não de 400**.**
+//  Não são bissextos todos os demais anos.
+//Com isso em mente, refatore a função para o Typescript.
+
+// function checaAnoBissexto(ano) {
+//   const cond1 = ano % 400 === 0
+//   const cond2 = (ano % 4 === 0) && (ano % 100 !== 0)
+//   return cond1 || cond2
+// }
+
+  function checaAnoBissexto(ano:number):boolean {
+    const cond1:boolean = ano % 400 === 0;
+    const cond2:boolean = (ano % 4 === 0) && (ano % 100 !== 0);
+    return cond1 || cond2;
+  };
+
+  console.log(checaAnoBissexto(2016));
