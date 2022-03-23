3776462d893096a169ad147b69e3dc66e7d513ee
Adria Tavares <adria.tavares28@gmail.com>
2022-03-23 12:44:31 -0300

    Exercício 2

--- modulo4/intro-typescript/src/index.ts
+++ modulo4/intro-typescript/src/index.ts
@@ -31,12 +31,14 @@ console.log(checaTriangulo(8, 5, 7));
 //   console.log([cor1, cor2, cor3])
 // }
 
-  // function imprimeTresCoresFavoritas():void {
-  //   const cor1:string = prompt("Insira sua primeira cor favorita")
-  //   const cor2:string = prompt("Insira sua segunda cor favorita")
-  //   const cor3:string = prompt("Insira sua terceira cor favorita")
-  //   console.log([cor1, cor2, cor3])
-  // }
+  function imprimeTresCoresFavoritas():void {
+    const cor1:string = process.argv[2]
+    const cor2:string = process.argv[3]
+    const cor3:string = process.argv[4]
+    console.log([cor1, cor2, cor3])
+  }
+
+  imprimeTresCoresFavoritas();
 
 //Exercício 3
 
