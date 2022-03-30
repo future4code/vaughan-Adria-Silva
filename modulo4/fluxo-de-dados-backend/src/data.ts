80e1eccd53a86e8a4ebd5687fc2d724713df4cc3
Adria Tavares <adria.tavares28@gmail.com>
2022-03-30 12:06:47 -0300

    Exercício 2

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,40 @@
+// Exercício 2
+
+type Product = {
+    id: string,
+    name: string,
+    price: number
+}
+
+export const data : Product[] = [
+    {
+        id: "1",
+        name: "camisa",
+        price: 30
+    },
+    {
+        id: "2",
+        name: "calça",
+        price: 81.99
+    },
+    {
+        id: "3",
+        name: "mochila",
+        price: 200
+    },
+    {
+        id: "4",
+        name: "agenda",
+        price: 5.75
+    },
+    {
+        id: "5",
+        name: "caderno",
+        price: 10.99
+    },
+    {
+        id: "6",
+        name: "caneta",
+        price: 2.50
+    }
+]
