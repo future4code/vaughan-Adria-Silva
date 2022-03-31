b69c6c61b1a2328db9019819fa17d4fb8c1b7be1
Adria Tavares <adria.tavares28@gmail.com>
2022-03-31 12:11:07 -0300

    Exercício 2

--- modulo4/apis-rest/src/index.ts
+++ modulo4/apis-rest/src/index.ts
@@ -20,6 +20,24 @@ app.get("/users", (req: Request, res: Response) => {
     };
 });
 
+// Exercício 2
+app.get("/users/:type", (req: Request, res: Response) => {
+    let errorCode: number = 400;
+    try {
+        const type: string = req.params.type;
+
+        if (type !== "ADMIN" && type !== "NORMAL") {
+            errorCode = 422;
+            throw new Error("Valor de type inválido. É necessário que type seja ADMIN ou NORMAL");
+        };
+
+        const filteredList = users.filter(user => user.type === type);
+        res.status(200).send(filteredList);
+    } catch (error: any) {
+        res.status(errorCode).send({message: error.message})
+    };
+});
+
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
        const address = server.address() as AddressInfo;
