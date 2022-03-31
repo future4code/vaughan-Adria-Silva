01a41ca55f83420d6e56f1ab56d8b17192a518c9
Adria Tavares <adria.tavares28@gmail.com>
2022-03-31 16:27:03 -0300

    Refatorando o código

--- modulo4/apis-rest/src/index.ts
+++ modulo4/apis-rest/src/index.ts
@@ -1,4 +1,4 @@
-import express, {Express, Request, Response} from 'express';
+import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import { AddressInfo } from "net";
 import { User, users } from './data';
@@ -13,58 +13,121 @@ app.use(cors());
 app.get("/users", (req: Request, res: Response) => {
     let errorCode: number = 500;
     try {
+        const name: string = req.query.name as string;
+        const type: string = req.query.type as string;
+        if (name) {
+            const user: User | undefined = users.find(user => user.name === name);
+            if (!user) {
+                errorCode = 404;
+                throw new Error("Usuário(a) não encontrado(a).");
+            };
+
+            res.status(200).send(user);
+        };
+
+        if (type) {
+            if (type !== "ADMIN" && type !== "NORMAL") {
+                errorCode = 422;
+                console.log("entrou");
+                throw new Error("Valor de type inválido. É necessário que type seja ADMIN ou NORMAL");
+            };
+    
+            const filteredList = users.filter(user => user.type === type);
+            res.status(200).send(filteredList);
+        }
+
         res.status(200).send(users);
 
-    } catch (error) {
-        res.status(errorCode).send({message: "Erro no servidor"})
+    } catch (error: any) {
+        if (errorCode === 500) {
+            res.status(errorCode).send({message: "Erro no servidor"})
+        }
+        res.status(errorCode).send({ message: error.message })
     };
 });
 
 // Exercício 2
-app.get("/users/:type", (req: Request, res: Response) => {
+// app.get("/users/:type", (req: Request, res: Response) => {
+//     let errorCode: number = 400;
+//     try {
+//         const type: string = req.params.type;
+
+//         if (type !== "ADMIN" && type !== "NORMAL") {
+//             errorCode = 422;
+//             throw new Error("Valor de type inválido. É necessário que type seja ADMIN ou NORMAL");
+//         };
+
+//         const filteredList = users.filter(user => user.type === type);
+//         res.status(200).send(filteredList);
+//     } catch (error: any) {
+//         res.status(errorCode).send({ message: error.message })
+//     };
+// });
+
+// Exercício 4
+app.post("/users", (req: Request, res: Response) => {
     let errorCode: number = 400;
     try {
-        const type: string = req.params.type;
+        const { name, email, type, age } = req.body;
 
+        if (!name || !email || !type || !age) {
+            errorCode = 422;
+            throw new Error("Dados incompletos");
+        }
         if (type !== "ADMIN" && type !== "NORMAL") {
             errorCode = 422;
             throw new Error("Valor de type inválido. É necessário que type seja ADMIN ou NORMAL");
         };
+        if (typeof age !== "number" || age <= 0) {
+            errorCode = 422;
+            throw new Error("Valor de age deve ser um número maior que zero.");
+        };
 
-        const filteredList = users.filter(user => user.type === type);
-        res.status(200).send(filteredList);
+        const id = Date.now();
+
+        const newUser: User = {
+            id,
+            name,
+            email,
+            type,
+            age
+        }
+
+        users.push(newUser);
+
+        res.status(201).send(users);
     } catch (error: any) {
-        res.status(errorCode).send({message: error.message})
+        res.status(errorCode).send({ messagem: error.message });
     };
 });
 
 // Exercício 3
-app.get("/user", (req: Request, res: Response) => {
-    let errorCode: number = 400;
-    try {
-        const name: string = req.query.name as string;
-        if (!name) {
-            errorCode = 422;
-            throw new Error("Nome não enviado");
-        };
+// app.get("/user", (req: Request, res: Response) => {
+//     let errorCode: number = 400;
+//     try {
+//         const name: string = req.query.name as string;
+//         if (!name) {
+//             errorCode = 422;
+//             throw new Error("Nome não enviado");
+//         };
 
-        const user: User | undefined =  users.find(user => user.name === name);
-        if (!user) {
-            errorCode = 404;
-            throw new Error("Usuário(a) não encontrado(a).");
-        };
+//         const user: User | undefined = users.find(user => user.name === name);
+//         if (!user) {
+//             errorCode = 404;
+//             throw new Error("Usuário(a) não encontrado(a).");
+//         };
 
-        res.status(200).send(user);
-    } catch (error: any) {
-        res.status(errorCode).send({message: error.message})
-    };
-});
+//         res.status(200).send(user);
+//     } catch (error: any) {
+//         res.status(errorCode).send({ message: error.message })
+//     };
+// });
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
-       const address = server.address() as AddressInfo;
-       console.log(`Server is running in http://localhost:${address.port}`);
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost:${address.port}`);
     } else {
-       console.error(`Failure upon starting server.`);
+        console.error(`Failure upon starting server.`);
     }
 });
