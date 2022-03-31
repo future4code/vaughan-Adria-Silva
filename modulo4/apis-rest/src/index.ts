4754ea7c52746039b9ffb94498625aac9bf40227
Adria Tavares <adria.tavares28@gmail.com>
2022-03-31 11:44:20 -0300

    Exercício 1

--- modulo4/apis-rest/src/index.ts
+++ modulo4/apis-rest/src/index.ts
@@ -1,6 +1,7 @@
-import express, {Express} from 'express';
+import express, {Express, Request, Response} from 'express';
 import cors from 'cors';
 import { AddressInfo } from "net";
+import { users } from './data';
 
 const app: Express = express();
 
@@ -8,11 +9,21 @@ app.use(express.json());
 app.use(cors());
 
 // ENDPOINTS
+// Exercício 1
+app.get("/users", (req: Request, res: Response) => {
+    let errorCode: number = 500;
+    try {
+        res.status(200).send(users);
+
+    } catch (error) {
+        res.status(errorCode).send({message: "Erro no servidor"})
+    };
+});
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
        const address = server.address() as AddressInfo;
-       console.log(`Server is running in http://localhost: ${address.port}`);
+       console.log(`Server is running in http://localhost:${address.port}`);
     } else {
        console.error(`Failure upon starting server.`);
     }
