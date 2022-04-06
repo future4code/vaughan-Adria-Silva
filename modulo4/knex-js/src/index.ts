be4a244256c7cecf6773c99cd74eb81ed8d18a77
Adria Tavares <adria.tavares28@gmail.com>
2022-04-06 17:36:28 -0300

    Exercício 1

--- modulo4/knex-js/src/index.ts
+++ modulo4/knex-js/src/index.ts
@@ -1,4 +1,4 @@
-import express, {Express, Request, Response} from 'express';
+import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import { AddressInfo } from "net";
 import knex from 'knex';
@@ -7,28 +7,74 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const connection = knex({
-	client: "mysql",
-	connection: {
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-  }
+    client: "mysql",
+    connection: {
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
+    }
 });
 
 const app: Express = express();
 app.use(express.json());
 app.use(cors());
 
-// ENDPOINTS
+// EXERCÍCIO 1
+// A) O raw responde com o resultado da query e outras infos que não interessam
+// para entregar como response da request, por isso utiliza-se o result[0]
+
+// B)
+
+const getActorByName = async (name: string): Promise<any> => {
+    const result = await connection.raw(`
+    SELECT * FROM Actor WHERE name = "${name}"
+    `);
+
+    return result[0];
+};
+
+const getNumberOfActors = async (gender: string): Promise<any> => {
+    const result = await connection.raw(`
+    SELECT COUNT(*) as quantity FROM Actor WHERE gender = "${gender}"
+    `);
+
+    return result[0];
+}
+
+app.get('/actor', async (req: Request, res: Response): Promise<void> => {
+    try {
+        const name = req.query.name;
+        const gender = req.query.gender;
+        let result: any = "Resultado"
+
+        if (name && typeof name === "string") {
+            result = await getActorByName(name);
+        } else if (gender && typeof gender === "string") {
+            result = await getNumberOfActors(gender);
+        } else {
+            const getAllActors = await connection.raw(`
+            SELECT * FROM Actor
+            `);
+            result = getAllActors[0];
+        };
+
+        res.status(200).send(result);
+    } catch (error: any) {
+        res.status(400).send(error.sqlMessage || error.message);
+    }
+
+});
+
+app.post("/actor")
 
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
-       const address = server.address() as AddressInfo;
-       console.log(`Server is running in http://localhost:${address.port}`);
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost:${address.port}`);
     } else {
-       console.error(`Failure upon starting server.`);
+        console.error(`Failure upon starting server.`);
     };
 });
