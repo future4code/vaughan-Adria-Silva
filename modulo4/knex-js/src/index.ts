e94753d7466128755989f1a7d9af676681f19ac0
Adria Tavares <adria.tavares28@gmail.com>
2022-04-06 15:59:13 -0300

    Configurando knex e conexão com o MySQL

--- modulo4/knex-js/src/index.ts
+++ modulo4/knex-js/src/index.ts
@@ -1,11 +1,27 @@
 import express, {Express, Request, Response} from 'express';
 import cors from 'cors';
 import { AddressInfo } from "net";
+import knex from 'knex';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const connection = knex({
+	client: "mysql",
+	connection: {
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  }
+});
 
 const app: Express = express();
-
 app.use(express.json());
 app.use(cors());
+
+// ENDPOINTS
 
 
 const server = app.listen(process.env.PORT || 3003, () => {
