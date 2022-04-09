8a54a13834f0645d3eccbdabbc7959b3e2e28497
Adria Tavares <adria.tavares28@gmail.com>
2022-04-09 17:20:31 -0300

    Endpoint Criar Usuário

--- modulo4/toDoList/src/connection.ts
+++ modulo4/toDoList/src/connection.ts
@@ -2,16 +2,15 @@ import knex from "knex";
 import dotenv from 'dotenv';
 
 dotenv.config();
+console.log(process.env.DB_PASS)
 
-const connection = knex({
+export const connection = knex({
     client: "mysql",
-    connection:{
+    connection: {
         host: process.env.DB_HOST,
-        port:3306,
+        port: 3306,
         user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_SCHEMA,
-        multiStatements: true
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
     }
-})
-export default connection; 
+});
--- modulo4/toDoList/src/index.ts
+++ modulo4/toDoList/src/index.ts
@@ -1,18 +1,56 @@
-import express, {Express} from 'express';
+import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import { AddressInfo } from "net";
-import connection from './connection';
+import {connection} from './connection';
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(cors());
 
+// FUNÇÔES 
+const createUser = async (id: number, name: string, nickname: string, email: string): Promise<void> => {
+   await connection("ToDoListUser")
+      .insert({
+         id,
+         name,
+         nickname,
+         email
+      });
+};
+
+// ENDPOINTS
+app.post("/user", (req: Request, res: Response) => {
+   let statusCode: number = 400;
+   try {
+      const { name, nickname, email } = req.body;
+
+      if (!name || !nickname || !email) {
+         statusCode = 422;
+         throw new Error("Informações para cadastro estão incompletas!");
+      };
+
+      const id = Date.now();
+      createUser(id, name, nickname, email);
+
+      const newUserInfo = {
+         id,
+         name,
+         nickname,
+         email
+      };
+
+      res.status(201).send({newUser: newUserInfo});
+   } catch (error: any) {
+      res.status(statusCode).send(error.sqlMessage || error.message); 
+   };
+});
+
 const server = app.listen(process.env.PORT || 3003, () => {
-    if (server) {
-       const address = server.address() as AddressInfo;
-       console.log(`Server is running in http://localhost:${address.port}`);
-    } else {
-       console.error(`Failure upon starting server.`);
-    };
+   if (server) {
+      const address = server.address() as AddressInfo;
+      console.log(`Server is running in http://localhost:${address.port}`);
+   } else {
+      console.error(`Failure upon starting server.`);
+   };
 });
