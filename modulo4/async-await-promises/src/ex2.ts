9d5c2e22ef2e81fd79f94aa94ea1cc1ac839c5ea
Adria Tavares <adria.tavares28@gmail.com>
2022-04-11 20:36:45 -0300

    Criando função main no Exercício 2

--- modulo4/async-await-promises/src/ex2.ts
+++ modulo4/async-await-promises/src/ex2.ts
@@ -3,20 +3,34 @@ import { baseURL } from "./baseURL"
 
 // Exercício 2
 
-// async function getSubscribers (): Promise<any[]> {
-    // const response = await axios.get(`${baseURL}/subscribers`);
-    // return response.data;
-//  };
-
-// b) Reescrevendo a função acima em arrow function:
- const getSubscribers = async(): Promise<any[]> => {
-    const response = await axios.get(`${baseURL}/subscribers`);
-    return response.data;
- };
-
  // a) A diferença é muito pouca. Em arrow funciton, getSubscribers é uma 
  //variável que recebe a resposta de uma função. Nesse caso a palavra
  //reservada async deve vir antes dos parâmetros dessa função, em (), e a 
  //tipagem do retorno deve vir logo depois dos (), assim como na função
  //nomeada.
 
+
+ // b) Reescrevendo a função abaixo em arrow function:
+
+//   async function getSubscribers (): Promise<any[]> {
+//      return axios
+//      .get(`${baseURL}/subscribers`)
+//      .then(res => res.data);  
+//   };
+
+const getSubscribers = async(): Promise<any[]> => {
+    return axios
+     .get(`${baseURL}/subscribers`)
+     .then(res => res.data);  
+ };
+ 
+ const main = async () => {
+    try {
+       const allSubscribers = await getSubscribers();
+       console.log(allSubscribers );
+    } catch (error: any) {
+       console.log(error.response?.data || error.message);
+    };
+ };
+ 
+ main();
