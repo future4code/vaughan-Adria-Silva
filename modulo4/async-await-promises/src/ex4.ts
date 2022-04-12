afe6fc7b279edee56eaacbd71524df19302810aa
Adria Tavares <adria.tavares28@gmail.com>
2022-04-11 21:28:31 -0300

    Função de criar notícia do Exercício 4

--- modulo4/async-await-promises/src/ex4.ts
+++ modulo4/async-await-promises/src/ex4.ts
@@ -1,23 +1,34 @@
 import axios from "axios"
 import { baseURL } from "./baseURL"
 
+// Exercício 4
+// a) Deve ser uma função assíncrona para não esperar a conclusão dessa
+//função para executar os próximos comandos;
+
+// b)
+
 type News = {
     title: string,
     content: string,
     date: number
 }
 
-const newNews = {
+const newNews: News = {
 	title: "Hamilton saiu do Frontend",
 	content: "Para a alegria de muitos, e a tristeza de alguns, a turma Hamilton está, agora, no backend!",
-	date: 1589818936000
+	date: Date.now()
 };
 
-// Exercício 4
-// a) Deve ser uma função assíncrona para não esperar a conclusão dessa
-//função para executar os próximos comandos;
-
-// b)
-const createNews = async (news: News): Promise<void>=> {
-    await axios.put(`${baseURL}/news`, news);
+const createNews = async (news: News): Promise<any>=> {
+    return axios.put(`${baseURL}/news`, news);
 };
+
+const main = async (): Promise<void> => {
+    try {
+       await createNews(newNews);
+    } catch (error: any) {
+       console.log(error.response?.data || error.message);
+    };
+ };
+ 
+ main();
