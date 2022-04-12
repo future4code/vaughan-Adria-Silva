6d1a77b83d68a398223e10acca4a4f8c5a1d8bf9
Adria Tavares <adria.tavares28@gmail.com>
2022-04-12 17:39:40 -0300

    Exercício 4

--- modulo4/filtros-ordenacao-paginacao/src/index.ts
+++ modulo4/filtros-ordenacao-paginacao/src/index.ts
@@ -82,10 +82,9 @@ export const getAllUsers = async (req: Request, res: Response): Promise<void> =>
 };
 
 // app.get("/users/", getAllUsers);
-app.get("/users/:type", getAllUsers);
+// app.get("/users/:type", getAllUsers);
 
 // EXERCÍCIO 3
-
 async function selectLimitedUsers(limit: number, offset: number): Promise<any> {
     const result = await connection("aula48_exercicio")
             .select("*")
@@ -121,4 +120,68 @@ export const getAllUsers3 = async (req: Request, res: Response): Promise<void> =
     }
 }
 
-app.get("/users/", getAllUsers3);
+// app.get("/users/", getAllUsers3);
+
+// EXERCÍCIO 4
+async function selectAllUsers4(limit: number, offset: number, name?: string, type?: string): Promise<any> {
+    let result: any;
+    if (name) {
+        console.log(limit, offset)
+        result = await connection("aula48_exercicio")
+            .select("*")
+            .where("name", "like", `%${name}%`)
+            .orderBy("name", "asc")
+            .limit(limit)
+            .offset(offset);
+    } else if (type) {
+        result = await connection("aula48_exercicio")
+            .select("*")
+            .where("type", "like", `%${type}%`)
+            .orderBy("name", "asc")
+            .limit(limit)
+            .offset(offset);
+    } else {
+        result = await connection("aula48_exercicio")
+            .select("*")
+            .orderBy("email", "asc")
+            .limit(limit)
+            .offset(offset);
+    };
+
+    return result;
+};
+
+export const getAllUsers4 = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const type = req.params.type;
+        const name = req.query.name as string;
+        let numPage: number = Number(req.query.page);
+
+        const quantityUsers: number = 5;
+        let offset = quantityUsers * (numPage - 1);
+        console.log(offset);
+
+        if (!numPage) {
+            numPage = 1;
+        };
+        if (numPage < 1) {
+            throw new Error("Deve ser informado um número de página acima de 1");
+        };
+
+        let users: User[] = await selectAllUsers4(quantityUsers, offset, name, type);
+        
+        if (!users.length) {
+            res.statusCode = 404
+            throw new Error("No users found")
+        }
+
+        res.status(200).send(users)
+
+    } catch (error: any) {
+        console.log(error)
+        res.send(error.message || error.sqlMessage)
+    }
+};
+
+app.get("/users/", getAllUsers4);
+app.get("/users/:type", getAllUsers4);
