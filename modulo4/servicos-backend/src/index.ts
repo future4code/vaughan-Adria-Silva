90c7339a844390df99c77ac1a2b20e34d7ae2375
Adria Tavares <adria.tavares28@gmail.com>
2022-04-13 14:42:39 -0300

    Exercício 1

--- modulo4/servicos-backend/src/index.ts
+++ modulo4/servicos-backend/src/index.ts
@@ -1,1 +1,15 @@
 // import app from "./app"
+
+import { getAddressInfo } from "./services/getAddressInfo";
+
+
+const main = async () => {
+    try {
+        const address = await getAddressInfo(60510196);
+        console.log(address);
+    } catch (error: any) {
+        console.log(error)
+    };
+};
+
+main();
---
+++ modulo4/servicos-backend/src/services/getAddressInfo.ts
@@ -0,0 +1,22 @@
+import axios from "axios"
+import { Address } from "../types"
+
+
+export const getAddressInfo = async (cep: number): Promise<Address | null> => {
+    try {
+        const result = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+
+        const address: Address = {
+            street: result.data.logradouro,
+            district: result.data.bairro,
+            city: result.data.localidade,
+            state: result.data.uf
+        };
+
+        return address;
+
+    } catch (error: any) {
+        console.log(error);
+        return null;
+    };
+}
